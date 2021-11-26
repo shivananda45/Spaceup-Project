e5f2a5ec34df9df7da2482ab2e00b332550abd3b
shivananda45 <reddyshivananda45@gmail.com>
2021-11-26 11:15:39 +0530

    Init

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import SpNavigator from './navigation/SpNavigator';
+
+export default function App() {
+  return (
+    <SpNavigator />
+  )
+}
---
+++ navigation/SpNavigator.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+import { enableScreens } from 'react-native-screens';
+
+enableScreens();
+
+import LoginScreen from '../screens/LoginScreen';
+import HomeScreen from '../screens/HomeScreen';
+
+
+const PrNavigator = () => {
+    const Stack = createStackNavigator();
+    const Tab = createBottomTabNavigator();
+    const stackScreens = () => {
+      <Stack.Navigator 
+      // screenOptions={{
+      //   headerBackTitleVisible: false,
+      //   headerTintColor: '#FFFFFF',
+      // }} 
+      >
+      <Stack.Screen name="Login" component={LoginScreen}
+                  options={
+                    ({ route }) => ({
+                      headerShown: true
+                    })
+                  }
+                />
+      <Stack.Screen name="Home" component={HomeScreen}
+                  options={
+                    ({ route }) => ({
+                      headerShown: true
+                    })
+                  }
+                />
+      </Stack.Navigator>
+    }
+    return (
+    <NavigationContainer>
+       {/* <Tab.Navigator>
+        <Tab.Screen name="" component={} />
+        <Tab.Screen name="" component={} />
+      </Tab.Navigator> */}
+    </NavigationContainer>
+    )
+} 
+
+                  
+export default PrNavigator;
