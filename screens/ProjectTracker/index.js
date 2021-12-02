746c8d62f1d832c859b7541e6cbf52ae6aa78cc0
vamshiofficial <vamshiofficial1@email.com>
2021-12-03 00:22:15 +0530

    phase 2.3.1

--- screens/ProjectTracker/index.js
+++ screens/ProjectTracker/index.js
@@ -50,31 +50,3 @@ const styles = StyleSheet.create({
 
 
 
-=======
-import { StyleSheet, Text, View } from 'react-native'
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import NewRequest from './NewRequest'
-import RequestHistory from './RequestHistory'
-const Tab = createMaterialTopTabNavigator();
-const PaymentTracking = () => {
-    return (
-        <Tab.Navigator
-            screenOptions={{
-                tabBarLabelStyle: { fontSize: 12, },
-                tabBarActiveTintColor: '#fff',
-                tabBarInactiveTintColor: 'rgba(255,255,255,0.5)',
-                tabBarStyle: { backgroundColor: '#393874', shadowOpacity: 0, elevation: 0 },
-                tabBarIndicatorStyle: { backgroundColor: 'white' }
-            }}
-        >
-            <Tab.Screen name="RequestHistory" component={RequestHistory}
-                name="Request History"
-            />
-            <Tab.Screen name="NewRequest" component={NewRequest}
-                name="New Request"
-            />
-        </Tab.Navigator>
-    )
-}
-export default PaymentTracking;
->>>>>>> Stashed changes
