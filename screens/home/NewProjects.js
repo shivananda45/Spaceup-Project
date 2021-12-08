c6774d4d6547d29ce5b48265b692b4500d6904f6
shivananda45 <reddyshivananda45@gmail.com>
2021-12-08 10:49:37 +0530

    added padding

--- screens/home/NewProjects.js
+++ screens/home/NewProjects.js
@@ -205,7 +205,7 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         alignItems:'center',
         justifyContent:'center',
-        marginRight: 10,
+        marginRight: 25,
     },
     headerLeftIconCon: {
         width: 40,
